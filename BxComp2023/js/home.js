bb5d51f832b08fe21611bdf930fe6d7167e88107
Gustavo Almeida <gustavo.jesus2806@gmail.com>
2023-08-09 01:26:24 -0300

    cria pasta bxcomp2023

---
+++ BxComp2023/js/home.js
@@ -0,0 +1,11 @@
+/* Scripts do WordPress e plugins */
+window._wpemojiSettings = {
+    // Configurações do emoji
+};
+
+(function (a, b, c) { /* Função de configuração do emoji */ })(window, document, window._wpemojiSettings);
+
+/* Scripts personalizados */
+(function (a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z) {
+    /* Código do script personalizado */
+})(jQuery);
