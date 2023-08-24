ff105e8bc748b0b2c80c0b4b28efc604ac0e99b7
Gustavo Almeida <gustavo.jesus2806@gmail.com>
2023-08-24 18:18:49 -0300

    add participantes page

--- BxComp2023/assets/js/select-team.js
+++ BxComp2023/assets/js/select-team.js
@@ -1,11 +1,17 @@
 const characters = document.querySelectorAll('.character');
 const characterImage = document.querySelector('.character-image img');
+const characterName = document.getElementById('character-name');
 
 characters.forEach(character => {
     character.addEventListener('click', () => {
         characters.forEach(c => c.classList.remove('selected'));
         character.classList.add('selected');
+
         const imgSrc = character.getAttribute('data-img');
+        const name = character.getAttribute('data-name');
+
         characterImage.setAttribute('src', imgSrc);
+        characterName.textContent = name;
     });
 });
+a
